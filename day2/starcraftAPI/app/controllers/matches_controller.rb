7f344a6e00516f1ef734918299eb7be28e9abd35
Maria <unamaria@gmail.com>
2015-07-21 15:41:02 +0200

    Create seeds

--- day2/starcraftAPI/app/controllers/matches_controller.rb
+++ day2/starcraftAPI/app/controllers/matches_controller.rb
@@ -1,3 +1,6 @@
 class MatchesController < ApplicationController
-
+	def index
+		matches = Match.joins(:players).where(winner_id: params[:player_id])
+		render json: matches
+	end
 end
--- day2/starcraftAPI/db/seeds.rb
+++ day2/starcraftAPI/db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+factions = ['Terrans', 'Zerg', 'Protoss']
+
+10.times do |i|
+	Match.create(winner_faction: factions.sample, loser_faction: factions.sample, duration: 1.5, date: Date.current)
+end
